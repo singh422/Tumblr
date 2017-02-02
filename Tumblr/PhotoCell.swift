49c7b3edc358fe63abb4591a90c65c9c8bc01d0a
Avinash <@purdue.edu>
2017-02-02 18:44:04 -0500

    Finished Tumblr lab

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Avinash Singh on 02/02/17.
+//  Copyright © 2017 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
+    }
+    
+    
+}
